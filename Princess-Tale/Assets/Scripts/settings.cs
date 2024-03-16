a61f4b48545d6434fb7facd68b9d51ab53a27166
marwa93nezar <meme.sa93@gmail.com>
2024-03-16 22:03:20 +0100

    create settings scene

---
+++ Princess-Tale/Assets/Scripts/settings.cs
@@ -0,0 +1,15 @@
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.Audio;
+public class settings : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    public void setVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+    public void goToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
