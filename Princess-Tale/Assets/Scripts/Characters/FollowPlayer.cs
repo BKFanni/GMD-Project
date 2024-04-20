b4c328ff3b8cfff4471ee14f987b4139c2083d7d
marwa93nezar <meme.sa93@gmail.com>
2024-04-20 23:22:03 +0200

    fix enemy movement + add conditions to switch between stats

--- Princess-Tale/Assets/Scripts/Characters/FollowPlayer.cs
+++ Princess-Tale/Assets/Scripts/Characters/FollowPlayer.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
@@ -14,48 +13,50 @@ public class FollowPlayer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-       playerPosition = player.GetComponent<Transform>();
-       currentPos = GetComponent<Transform>().position;
-       animator = GetComponent<Animator>();
+        playerPosition = player.GetComponent<Transform>();
+        currentPos = GetComponent<Transform>().position;
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+
         FollowTarget();
     }
 
 
     void FollowTarget()
     {
-         if(Vector2.Distance(transform.position, playerPosition.position) < distance && Vector2.Distance(transform.position, playerPosition.position) > targetpos)
+        if (Vector2.Distance(transform.position, playerPosition.position) < distance && Vector2.Distance(transform.position, playerPosition.position) > targetpos)
         {
             transform.position = Vector2.MoveTowards(transform.position, playerPosition.position, speed * Time.deltaTime);
-            animator.SetBool("Running",true);
+            animator.SetBool("Running", true);
         }
-        else{
-            if(Vector2.Distance(transform.position, currentPos) <= 1)
+        else
+        {
+            if (Vector2.Distance(transform.position, currentPos) <= 1)
             {
                 animator.SetBool("Running", false);
             }
-            else{
+            else
+            {
                 transform.position = Vector2.MoveTowards(transform.position, currentPos, speed * Time.deltaTime);
-                animator.SetBool("Running",true);
-              }
+                animator.SetBool("Running", true);
+            }
         }
         FlipSprite();
     }
 
     void FlipSprite()
     {
-        if(player.position.x > transform.position.x)
+        if (player.position.x > transform.position.x)
         {
-            transform.localScale = new Vector3(1,1,1);
+            transform.localScale = new Vector3(1, 1, 1);
         }
-        else if(player.position.x < transform.position.x)
+        else if (player.position.x < transform.position.x)
         {
-            transform.localScale = new Vector3(-1,1,1);
+            transform.localScale = new Vector3(-1, 1, 1);
         }
     }
 
