adedf061d288c88e356252142d518ae93f17bde6
BKFanni <klaufb02@gmail.com>
2024-04-27 19:08:27 +0200

    Create Level2 + Set player position + Add new assets
    Set player position to spawn at the beginning of the level in the next scene. Also organized the files a bit: Moved the fireball assets to the artwork folder

---
+++ Princess-Tale/Assets/Scripts/SetPlayerPosition.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Build.Content;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SetPlayerPosition : MonoBehaviour
+{
+    
+    [SerializeField] private Vector3 startPos;
+ 
+    private void Awake()
+    {
+        GameObject player = GameObject.FindWithTag("Player");
+        player.transform.position = startPos;
+    }
+}
