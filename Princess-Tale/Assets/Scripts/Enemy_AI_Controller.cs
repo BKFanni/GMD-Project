6860e069d2be940f27344956e86010c478b9f6df
BKFanni <klaufb02@gmail.com>
2024-05-07 22:53:51 +0200

    Fix enemy return script + modify enemy movement

--- Princess-Tale/Assets/Scripts/Enemy_AI_Controller.cs
+++ Princess-Tale/Assets/Scripts/Enemy_AI_Controller.cs
@@ -11,14 +11,20 @@ public class Enemy_AI_Controller : MonoBehaviour
     private FollowPlayer followPlayer;
     private  EnemyMovement enemyMovement;
     private  EnemyAttack enemyAttack;
+    private Return enemyReturn;
+    public Transform pointA;
+    public Transform pointB;
 
     // Start is called before the first frame update
     void Start()
     {
         followPlayer = GetComponent<FollowPlayer>();
         enemyMovement = GetComponent<EnemyMovement>();
+        enemyReturn = GetComponent<Return>();
         playerPosition = player.GetComponent<Transform>();
         enemyAttack = GetComponent<EnemyAttack>();
+        pointA = GetComponent<Transform>();
+        pointB = GetComponent<Transform>();
     }
 
     // Update is called once per frame
@@ -29,9 +35,10 @@ public class Enemy_AI_Controller : MonoBehaviour
 
     void ControlEnemy()
     {
-        if (Vector2.Distance(transform.position, playerPosition.position) < distance && Vector2.Distance(transform.position, playerPosition.position) > 2)
+        if (Vector2.Distance(transform.position, playerPosition.position) <= distance && Vector2.Distance(transform.position, playerPosition.position) > 2)
         {
             enemyAttack.enabled = false;
+            enemyReturn.enabled = false;
             enemyMovement.enabled = false;
             followPlayer.enabled = true;
             
@@ -39,16 +46,25 @@ public class Enemy_AI_Controller : MonoBehaviour
         else if (Vector2.Distance(transform.position, playerPosition.position) >= 0.1f && Vector2.Distance(transform.position, playerPosition.position) <= 2)
         {
             followPlayer.enabled = false;
+            enemyReturn.enabled=false;
             enemyMovement.enabled = false;
             enemyAttack.enabled = true;
             
+        }
+        else if (transform.position.x >= pointA.position.x && transform.position.x <= pointB.position.x) 
+        {
+            enemyAttack.enabled = false;
+            enemyReturn.enabled=true;
+            followPlayer.enabled = false;
+            enemyMovement.enabled = true;
+          
         }
         else
         {
             enemyAttack.enabled = false;
             followPlayer.enabled = false;
-            enemyMovement.enabled = true;
-          
+            enemyMovement.enabled =false;
+            enemyReturn.enabled=true;
         }
     }
 }
