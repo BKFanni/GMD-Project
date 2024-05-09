cbc43407084d2710c54c6d10b271d2ce18512050
marwa93nezar <meme.sa93@gmail.com>
2024-05-09 21:59:25 +0200

    Add Progress bar script to control the bar fill amount

---
+++ Princess-Tale/Assets/Scripts/progressBar.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class ProgressBar : MonoBehaviour
+{
+    public Image fillBar;
+    public Transform player;
+    public Transform levelFinishPoint;
+    public TMP_Text levelNumberTextLeft;
+    public TMP_Text levelNumberTextRight;
+
+    private int currentLevel = 1;
+
+    void Update()
+    {
+        float distanceToFinish = Vector3.Distance(player.position, levelFinishPoint.position);
+        float totalDistance = Vector3.Distance(transform.position, levelFinishPoint.position);
+        float progressPercentage = 1f - (distanceToFinish / totalDistance);
+
+        fillBar.fillAmount = progressPercentage;
+
+        levelNumberTextLeft.text = currentLevel.ToString();
+        levelNumberTextRight.text = (currentLevel + 1).ToString();
+    }
+
+    public void SetCurrentLevel(int level)
+    {
+        currentLevel = level;
+    }
+}
