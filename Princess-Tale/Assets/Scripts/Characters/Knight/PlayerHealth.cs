c1f622d3a7bd4b57c4425257443bc5527878fc4a
marwa93nezar <meme.sa93@gmail.com>
2024-05-14 19:56:01 +0200

    Create script for player Health

---
+++ Princess-Tale/Assets/Scripts/Characters/Knight/PlayerHealth.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+
+    public int maxLives = 3;
+    public float maxHealthPerLife = 100f;
+    public float currentHealth;
+    public int currentLives;
+    public Health healthScript;
+    public Animator animator;
+
+    void Start()
+    {
+        healthScript = GetComponent<Health>();
+        animator = GetComponent<Animator>();
+        currentLives = maxLives;
+        currentHealth = maxHealthPerLife;
+        if (healthScript != null)
+        {
+            UpdateHealthUI();
+        }
+    }
+
+    public void TakeDamage(float damageAmount)
+    {
+        currentHealth -= damageAmount;
+        if (currentHealth <= 0)
+        {
+            currentLives--;
+            if (currentLives > 0)
+            {
+                currentHealth = maxHealthPerLife;
+            }
+            else
+            {
+                Die();
+                return;
+            }
+        }
+
+        UpdateHealthUI();
+    }
+
+    void Die()
+    {
+        if (animator != null)
+        {
+            animator.SetBool("Die", true);
+        }
+    }
+
+    public void UpdateHealthUI()
+    {
+        if (healthScript != null)
+        {
+            healthScript.health = currentHealth;
+            healthScript.numOfHearts = currentLives;
+        }
+    }
+}
